9cc4ad5b40d4143ffc77a3599adba3cbf29fcd3a
jvblum <jvmell.skch@gmail.com>
2021-10-26 17:18:53 -0400

    style DayListItem component

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,10 +1,22 @@
 import React from "react";
 
+import "components/DayListItem.scss";
+import classNames from "classnames";
+
 export default function DayListItem(props) {
+  const {
+    name,
+    spots,
+    setDay,
+    selected
+  } = props;
+
+  const dayListClass = classNames('day-list__item', { 'day-list__item--selected': selected }, { 'day-list__item--full': !spots });
+
   return (
-    <li>
-      <h2 className="text--regular">Day Name</h2> 
-      <h3 className="text--light">X spots remaining</h3>
+    <li onClick={() => setDay(name)} className={dayListClass}>
+      <h2 className="text--regular">{name}</h2> 
+      <h3 className="text--light">{spots} spots remaining</h3>
     </li>
   );
 }
