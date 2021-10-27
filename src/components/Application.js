483a2ac08e7e719c70de5124b2c093f94921dcf4
jvblum <jvmell.skch@gmail.com>
2021-10-27 09:51:25 -0400

    implement InterviewerListItem component

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,12 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "components/Application.scss";
 import Button from "./Button";
+import DayList from "./DayList";
 
 export default function Application(props) {
-  const testEvent = () => {
-    alert('hey');
-  }
+  const [daySelect, setDaySelect] = useState('Monday');
+
+  const days = [
+    {
+      id: 1,
+      name: "Monday",
+      spots: 2,
+    },
+    {
+      id: 2,
+      name: "Tuesday",
+      spots: 5,
+    },
+    {
+      id: 3,
+      name: "Wednesday",
+      spots: 0,
+    },
+  ];
+
   return (
     <main className="layout">
       <section className="sidebar">
@@ -16,7 +34,13 @@ export default function Application(props) {
           alt="Interview Scheduler"
         />
         <hr className="sidebar__separator sidebar--centered" />
-        <nav className="sidebar__menu"></nav>
+        <nav className="sidebar__menu">
+          <DayList
+            days={days}
+            day={daySelect}
+            setDay={setDaySelect}
+          />
+        </nav>
         <img
           className="sidebar__lhl sidebar--centered"
           src="images/lhl.png"
@@ -27,7 +51,7 @@ export default function Application(props) {
       <section className="schedule">
         <Button confirm>hey</Button>
         <Button danger>you</Button>
-        <Button onClick={testEvent}>onClick</Button>
+        {/* <Button onClick={testEvent}>onClick</Button> */}
         <Button disabled>off</Button>
         {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
       </section>
