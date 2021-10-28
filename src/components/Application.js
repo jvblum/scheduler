619e395e332bf27280fd612adf17c477881c9fc1
jvblum <jvmell.skch@gmail.com>
2021-10-28 18:06:30 -0400

    init implement axios

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
 
 import "components/Application.scss";
 import Button from "./Button";
@@ -9,24 +10,15 @@ import appointments from "data/data";
 
 export default function Application(props) {
   const [daySelect, setDaySelect] = useState('Monday');
+  const [days, setDays] = useState([]);
 
-  const days = [
-    {
-      id: 1,
-      name: "Monday",
-      spots: 2,
-    },
-    {
-      id: 2,
-      name: "Tuesday",
-      spots: 5,
-    },
-    {
-      id: 3,
-      name: "Wednesday",
-      spots: 0,
-    },
-  ];
+
+  // Create an effect to make a GET request to /api/days using axios and update the days state with the response.
+  useEffect(() => {
+    axios.get('/api/days').then(res => {
+      setDays(res.data);
+    });
+  }, []);
 
   const parsedappointments = appointments.map(appointment => <Appointment key={appointment.id} {...appointment}/>);
 
